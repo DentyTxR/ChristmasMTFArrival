107b5cc12db9b7b62a1cb866bfa4886eca9f2424
Denty <dentymods.axuka@gmail.com>
2022-12-05 12:46:45 -0500

    fixed issue where glitch effect overlaps (aka lowerd the chance)

--- ChristmasMTFArrival/EventHandler.cs
+++ ChristmasMTFArrival/EventHandler.cs
@@ -13,7 +13,7 @@ namespace ChristmasMTFArrival
                 Log.Debug("Sending noSCPs Christmas MTF arrival cassie");
 
                 ev.IsAllowed = false;
-                Cassie.GlitchyMessage($"xmas_epsilon11 nato_{ev.UnitName[0]} {ev.UnitNumber} xmas_hasentered NoSCPsLeft", 0.5f, 0.5f);
+                Cassie.GlitchyMessage($"xmas_epsilon11 nato_{ev.UnitName[0]} {ev.UnitNumber} xmas_hasentered NoSCPsLeft", 0.05f, 0.05f);
                 return;
             }
             if (ev.ScpsLeft > 0)
@@ -21,7 +21,7 @@ namespace ChristmasMTFArrival
                 Log.Debug("Sending SCPsLeft Christmas MTF arrival cassie");
 
                 ev.IsAllowed = false;
-                Cassie.GlitchyMessage($"xmas_epsilon11 nato_{ev.UnitName[0]} {ev.UnitNumber} xmas_hasentered {ev.ScpsLeft} xmas_scpsubjects", 0.5f, 0.5f);
+                Cassie.GlitchyMessage($"xmas_epsilon11 nato_{ev.UnitName[0]} {ev.UnitNumber} xmas_hasentered {ev.ScpsLeft} xmas_scpsubjects", 0.05f, 0.05f);
                 return;
             }
         }
